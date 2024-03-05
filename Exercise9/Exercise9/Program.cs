61726a14ca6bec0e5da44eaaaa2d51a453753595
Brianna Rodriguez <145356670+BriesIssues@users.noreply.github.com>
2024-03-05 10:12:34 -0500

    Ready for grading

--- Exercise9/Exercise9/Program.cs
+++ Exercise9/Exercise9/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            //prompt 1
+            //width of row 
+            Console.Write("width of rows: ");
+            int widthOfRow = int.Parse(Console.ReadLine());
+
+            // number of rows
+            Console.Write("number of rows: ");
+            int numberOfRow = int.Parse(Console.ReadLine());
+
+            //correct numbers of astrisks (*)
+            for (int i = 0; i < widthOfRow; i++)
+            {
+                for (int j = 0; j < numberOfRow; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            //Prompt 2
+            //row width
+            Console.WriteLine("row width: ");
+            int rowWidth = int.Parse(Console.ReadLine());
+            
+            //row of askriskes adding 1 (*) for eatch row
+            for (int t = 0; t <= rowWidth; t++)
+            {
+               for (int f = 0; f <= t; f++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }       
         }
     }
 }
